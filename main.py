ee25bb0e8ba557de702534efa1d044bdac5467e0
ffdd270 <ffdd270@gmail.com>
2020-10-31 21:48:18 +0900

    Basic Tag Getter Impl.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import TextIO
 from notion.client import NotionClient
+from notion.collection import CollectionView, Collection, CollectionRowBlock
 from io import TextIOWrapper
 
 
@@ -17,16 +18,57 @@ def get_token(io: TextIO) -> str:
     return split_result[1]
 
 
+def get_tags_count(notion_client: NotionClient, path: str, tag_name: str) -> dict:
+    collection_view: CollectionView = notion_client.get_collection_view(path)
+    collection: Collection = collection_view.collection
+
+    schema: list = None
+    name_to_slug: str = None
+    tags_dict: dict = {}
+
+    for row in collection.get_rows():
+        collection_row: CollectionRowBlock = row
+
+        if schema is None:
+            schema = collection_row.schema
+
+            for schema_item in schema:
+                if schema_item['name'] == tag_name:
+                    name_to_slug = schema_item['slug']
+                    break
+
+            if name_to_slug is None:
+                print("Maybe, that is wrong property.")
+                return {}
+
+        tags: list[str] = getattr(collection_row, name_to_slug)
+
+        for tag in tags:
+            if tags_dict.get(tag) is None:
+                tags_dict[tag] = 0
+            tags_dict[tag] += 1
+
+    return tags_dict
+
+
 account: TextIO = open("notion_account.env", "r")
 token = get_token(account)
 
 # Obtain the `token_v2` value by inspecting your browser cookies on a logged-in (non-guest) session on Notion.so
-client = NotionClient(token_v2="<" + token + ">")
+client = NotionClient(token_v2=token)
 
 # Replace this URL with the URL of the page you want to edit
-page = client.get_block("https://www.notion.so/sihawn/")
 
-print("The old title is:", page.title)
+tags = get_tags_count(client,
+                      "https://www.notion.so/sihawn/9600ea9d0583409d8d914807ec58f253?v=aae30af392e84573b1a53ee39d531f61",
+                      "태그");
 
-# Note: You can use Markdown! We convert on-the-fly to Notion's internal formatted text data structure.
-#page.title = "The title has now changed, and has *live-updated* in the browser!"
+sort_tag: [dict] = []
+
+for k in tags:
+    sort_tag.append({'key': k, 'value': tags[k]})
+
+sort_tag.sort(key=lambda v: v['value'], reverse=True)
+
+for v in sort_tag:
+    print(v)
